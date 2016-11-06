9ae6292671a0cb7f57ab6e4e4d9af4551f757020
Коньков Алексей Юрьевич <KONKOV5471@belgiprozem.local>
2016-11-06 04:45:29 +0300

    collector works

--- other/collector.py
+++ other/collector.py
@@ -3,15 +3,15 @@
 
 import os
 import openpyxl
-import pyodbc
+from DbTools import DBConn
 from dbfread import DBF
 
 from os import listdir
-from os.path import isfile, join
+from os import path
 project_dir = os.getcwd()
 
 def is_valid_xl_file(f):
-    if not isfile(join(project_dir, f)):
+    if not path.isfile(path.join(project_dir, f)):
         return False
     if not (f.lower()[-4:] == u'xlsx' or f.lower()[-3:] == u'xls'):
         return False
@@ -48,7 +48,6 @@ def get_sheet_by_name(work_book, sh_name = None):
 def get_dbf_table(file_path):
     try:
         db = DBF(file_path)
-        print db
     except Exception as err:
         print err
         raise err
@@ -69,7 +68,7 @@ def getMdbStructureFromXls(xls_file_path):
 
     available_types = {
         'Long': 'INTEGER',
-        'Floa': 'DOUBLE',
+        'Floa': 'REAL',
         'Shor': 'SMALLINT',
         'Text': 'VARCHAR',
 
@@ -83,13 +82,14 @@ def getMdbStructureFromXls(xls_file_path):
         sheet = get_sheet_by_name(wb, sh_name)
         structure_doc[sh_name] = {
             'tab_name': sheet.cell(u'B1').value,
-            'NPRIZNAK': {
-                'dbf_field': 'NPRIZNAK',
-                'mdb_type': 'VARCHAR'
+            'tab_fields': ['NPRIZNAK', 'CTITLE'],
+            'dbf_fields': {
+                'NPRIZNAK': 'NPRIZNAK',
+                'CTITLE': 'CTITLE',
             },
-            'CTITLE': {
-                'dbf_field': 'CTITLE',
-                'mdb_type': 'VARCHAR'
+            'mdb_types': {
+                'NPRIZNAK': 'VARCHAR',
+                'CTITLE': 'VARCHAR',
             }
         }
 
@@ -110,29 +110,128 @@ def getMdbStructureFromXls(xls_file_path):
             except TypeError:
                 print 'warning on table' + sh_name
                 mdb_type = 'VARCHAR'
-            structure_doc[sh_name][field_name] = {
-                'dbf_field': dbf_field,
-                'mdb_type': mdb_type
-            }
+            structure_doc[sh_name]['tab_fields'].append(field_name)
+            structure_doc[sh_name]['dbf_fields'][field_name] = dbf_field
+            structure_doc[sh_name]['mdb_types'][field_name] = mdb_type
             f_cell_num += 1
     return structure_doc
 
 
-def getDbfFileNames(fls_path):
+def get_dbf_file_names(fls_path):
 
     def is_valid_dbf_file(f):
-        if not isfile(join(project_dir, f)):
+        if not path.isfile(path.join(fls_path, f)):
             return False
         if not f.lower()[-3:] == u'dbf':
             return False
         return True
 
-    return[f for f in listdir(fls_path ) if is_valid_dbf_file(f)]
+    return [f for f in listdir(fls_path ) if is_valid_dbf_file(f)]
 
 
-def collectDataToMdb(dbf_path, mdb_str):
-    dbf_files = getDbfFileNames(dbf_path)
-    print dbf_files
+def get_table_type(dbf_name):
+    tab_type = dbf_name.split('_')[0]
+    if not tab_type:
+        raise Exception('Can\'t identify table type')
+    tab_type = tab_type.upper()
+    if tab_type.split('0')[0] == 'T':
+        tab_type = ''.join(tab_type.split('0'))
+    return tab_type
+
+def get_table_code(dbf_name):
+    tab_code = dbf_name.split('_')[1]
+    return tab_code.split('.')[0]
+
+
+
+def handle_int(val):
+    try:
+        return int(val)
+    except Exception:
+        raise
+
+
+def handle_float(val):
+    try:
+        return float(val)
+    except Exception:
+        raise Exception('Unsupported type')
+
+def synchronize_by_type(val, type):
+    check_types = {
+        'INTEGER': lambda x: handle_int(x),
+        'REAL': lambda x: handle_float(x),
+        'SMALLINT': lambda x: handle_int(x),
+        'VARCHAR': lambda x: unicode(x),
+    }
+    return check_types[type](val)
+
+
+def collect_data_to_mdb(dbf_path, mdb_path, required_mdb_str):
+    dbf_files_names = get_dbf_file_names(dbf_path)
+    mdb_db = DBConn(mdb_path)
+    mdb_out_structure = mdb_db.get_tabs_with_field_names()
+
+
+    for tab_type in required_mdb_str:
+        should_be_t_name = required_mdb_str[tab_type]['tab_name']
+        should_be_t_fields = required_mdb_str[tab_type]['tab_fields']
+        if should_be_t_name not in mdb_out_structure:
+            raise Exception('table for %s is not found in out mdb database' % tab_type)
+        for field in should_be_t_fields:
+            required_f_type = required_mdb_str[tab_type]['mdb_types'][field]
+            if field not in mdb_out_structure[should_be_t_name]:
+                print(' adding field %s to table %s' % (field, should_be_t_name))
+                # add new field
+                mdb_db.add_field_if_not_exists(should_be_t_name, field, required_f_type)
+            elif mdb_out_structure[should_be_t_name][field] != required_f_type:
+                if required_f_type in ['SMALLINT', 'INTEGER']:
+                    continue
+                else:
+                    raise Exception('%s field type for table %s doesnt match with requirement' % (field, should_be_t_name))
+
+    del mdb_out_structure
+
+    for dbf_name in dbf_files_names:
+        tab_type = get_table_type(dbf_name)
+        tab_code = get_table_code(dbf_name)
+        if not required_mdb_str.has_key(tab_type):
+            print 'Faild to specify dbf file %s with table in excel structure' % dbf_name
+            continue
+        if not tab_code:
+            print 'There is no code for dbf file ' + dbf_name
+            continue
+
+        dbf_data = get_dbf_table(path.join(dbf_path, dbf_name))
+        tab_name = required_mdb_str[tab_type]['tab_name']
+        ordered_fields = required_mdb_str[tab_type]['tab_fields']
+        dbf_fields = required_mdb_str[tab_type]['dbf_fields']
+        mdb_types = required_mdb_str[tab_type]['mdb_types']
+        one_time_flag = False
+        for row in dbf_data:
+            insert_values = []
+            for f in ordered_fields:
+                f_val = ''
+                try:
+                    f_val = row[dbf_fields[f]]
+                except KeyError:
+                    if not one_time_flag:
+                        one_time_flag = True
+                        print 'for dbf %s field %s was not found' % (dbf_name, f)
+                #TODO: make fileds that are comes not from dbf
+                if f == 'NR_User':
+                    f_val = tab_code
+
+                # synchronizing with type
+                f_val = synchronize_by_type(f_val, mdb_types[f])
+                insert_values.append(f_val)
+            mdb_db.insert_row(tab_name, ordered_fields, insert_values)
+
+
+
+
+
+
 
 
 if __name__ == u'__main__':
@@ -140,6 +239,6 @@ if __name__ == u'__main__':
     mdb_out_file = project_dir + '\\ProbaCO6258.mdb'
     dbf_fls_path = project_dir + '\\dbf_files'
     xls_str = getMdbStructureFromXls(xls_str_file)
-    collectDataToMdb(dbf_fls_path, xls_str)
-    # get_dbf_table(project_dir + '\\t02_6258000005.dbf')
-    # run_searched_files(xls_files)
+    collect_data_to_mdb(dbf_fls_path, mdb_out_file, xls_str)
+
+    #TODO: make a copy of mdb file
