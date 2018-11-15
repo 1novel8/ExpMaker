c3260ce2768be1ce50b4265e4416a1a1f154ca5f
Alex Konkov <alex_konkov@tut.by>
2018-11-16 02:07:00 +0300

    optimized setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,34 @@
 import sys
 from cx_Freeze import setup, Executable
 
-base = "Win32GUI"
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
 
-executables = [Executable("Explication.py", base=base)]
+executables = [Executable('Explication.py', base=base)]
 
 packages = [
-    "sip", "threads", "uiWidgets", "os", "decimal", "datetime",
-    "sys", "pyodbc", "PyQt5", 'PyQt5.QtGui', 'PyQt5.QtWidgets',
+    'sip', 'threads', 'uiWidgets', 'os', 'decimal', 'datetime',
+    'sys', 'pyodbc', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtWidgets',
 ]
 options = {
     'build_exe': {
-        'packages': packages,
+        # 'packages': packages,
         'include_msvcr': True,
-        'compressed': True,
+        'optimize': 2,
+        'excludes': ['tkinter'],
+        # 'zip_include_packages': '*'
         # 'include_files': [
         #     (r'C:\Windows\System32\msvcr100.dll', 'msvcr100.dll'),
         # ],
-        # "exclude_dlls": ["MSVCP90.dll", "CRYPT32.dll", "COMDLG32.dll", "IMM32.dll", "ole32.dll", "WINSPOOL.DRV", "WINMM.dll", "USER32.dll", "SHELL32.dll", "ODBC32.dll", "ADVAPI32.dll", "WS2_32.dll", "GDI32.dll", "KERNEL32.dll"],            "bundle_files": 1,
+        # 'exclude_dlls': ['MSVCP90.dll', 'CRYPT32.dll', 'COMDLG32.dll', 'IMM32.dll', 'ole32.dll', 'WINSPOOL.DRV', 'WINMM.dll', 'USER32.dll', 'SHELL32.dll', 'ODBC32.dll', 'ADVAPI32.dll', 'WS2_32.dll', 'GDI32.dll', 'KERNEL32.dll'],            'bundle_files': 1,
     },
 }
 
 setup(
-    name="XConverter",
+    name='Explication 2.0',
     options=options,
-    version="1.0.0",
-    description='Media encryption tool',
+    version='2.0.0',
+    description='Explication maker tool',
     executables=executables
 )
